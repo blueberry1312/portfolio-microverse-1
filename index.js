0d812601b11e4c688a0ecaa305f55d731ad4f3b0
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-01-31 14:03:55 -0300

    Add popupWindow array

--- index.js
+++ index.js
@@ -5,6 +5,89 @@ const portfolio = document.querySelector('#portfolio-mobile');
 const about = document.querySelector('#about-mobile');
 const contact = document.querySelector('#contact-mobile');
 const closeBtn = document.querySelector('.btn-close-nav');
+const popupBtn = document.querySelector('.button-project');
+const list = document.getElementById('myList');
+
+const popupWindow = [
+  {
+    id: '0',
+    name: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    featuredImage: 'assets/img/Snapshoot.svg',
+    technologies: {
+      tech1: 'Ruby on rails',
+      tech2: 'css',
+      tech3: 'Javscript',
+    },
+    liveBtn: 'See Live',
+    sourceBtn: 'See Source',
+  },
+  {
+    id: '1',
+    name: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    featuredImage: 'assets/img/Snapshoot.svg',
+    technologies: {
+      tech1: 'Ruby on rails',
+      tech2: 'css',
+      tech3: 'Javscript',
+    },
+    liveBtn: 'See Live',
+    sourceBtn: 'See Source',
+  },
+  {
+    id: '2',
+    name: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    featuredImage: 'assets/img/Snapshoot.svg',
+    technologies: {
+      tech1: 'Ruby on rails',
+      tech2: 'css',
+      tech3: 'Javscript',
+    },
+    liveBtn: 'See Live',
+    sourceBtn: 'See Source',
+  },
+  {
+    id: '3',
+    name: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    featuredImage: 'assets/img/Snapshoot.svg',
+    technologies: {
+      tech1: 'Ruby on rails',
+      tech2: 'css',
+      tech3: 'Javscript',
+    },
+    liveBtn: 'See Live',
+    sourceBtn: 'See Source',
+  },
+  {
+    id: '4',
+    name: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    featuredImage: 'assets/img/Snapshoot.svg',
+    technologies: {
+      tech1: 'Ruby on rails',
+      tech2: 'css',
+      tech3: 'Javscript',
+    },
+    liveBtn: 'See Live',
+    sourceBtn: 'See Source',
+  },
+  {
+    id: '5',
+    name: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    featuredImage: 'assets/img/Snapshoot.svg',
+    technologies: {
+      tech1: 'Ruby on rails',
+      tech2: 'css',
+      tech3: 'Javscript',
+    },
+    liveBtn: 'See Live',
+    sourceBtn: 'See Source',
+  },
+];
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.add('hidden');
@@ -34,4 +117,10 @@ contact.addEventListener('click', () => {
   hamburger.classList.remove('hidden');
   logoPlaceholder.classList.remove('hidden');
   navMobile.classList.add('hidden');
+});
+
+popupBtn.addEventListener('click', () => {
+  popupWindow.forEach(() => {
+  
+  });
 });
